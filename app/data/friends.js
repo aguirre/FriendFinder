44ef46dd01a86d374b2e5110ce2d2c89a87b0d37
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-11-19 18:01:22 -0800

    update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,32 @@
 var friends = [
   {
-    name: "Ahmed",
-    photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    name: "Jim",
+    photo: "https://vignette.wikia.nocookie.net/theoffice/images/9/9a/Jim.jpg",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Pam",
+    photo:
+      "https://vignette.wikia.nocookie.net/theoffice/images/6/67/Pam_Beesley.jpg",
+    scores: [3, 2, 5, 1, 2, 3, 3, 2, 1, 5]
+  },
+  {
+    name: "Michael",
+    photo:
+      "https://vignette.wikia.nocookie.net/theoffice/images/0/02/Michael_Scott.jpg",
+    scores: [1, 2, 1, 4, 3, 5, 2, 1, 2, 4]
+  },
+  {
+    name: "Dwight",
+    photo:
+      "https://vignette.wikia.nocookie.net/theoffice/images/c/c5/Dwight_.jpg",
+    scores: [3, 2, 3, 1, 2, 5, 3, 4, 1, 2]
+  },
+  {
+    name: "Angela",
+    photo:
+      "https://vignette.wikia.nocookie.net/theoffice/images/0/0b/Angela_Martin.jpg",
+    scores: [5, 2, 3, 1, 4, 3, 1, 4, 5, 5]
   }
 ];
 
